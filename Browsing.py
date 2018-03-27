6741b9177a5fda2efb3fcd3041befc2114c84b81
victor <victor_souzas@yahoo.com.br>
2018-03-27 08:33:33 -0300

    started google request

--- Browsing.py
+++ Browsing.py
@@ -1,13 +1,22 @@
 # -*- coding: utf-8 -*-
 
+
 import requests
 
 
 class Browsing(object):
+    """doc string for Browsing"""
 
     def __init__(self):
+        super(Browsing, self).__init__()
         self.session = requests.Session()
 
+    def start(self):
+        google = self.session.get('http://www.google.com')
+        if google.status_code is 200:
+            cookies = google.cookies
 
-if __name__ is "__main__":
+
+if __name__ == "__main__":
     browser = Browsing()
+    browser.start()
